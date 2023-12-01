b97277f22bbc4ca8c7cc9f542193c8439613fda0
Abdullah Yaser <38732824+buudi@users.noreply.github.com>
2023-12-01 16:35:35 +0800

    config

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    user: "postgres",
-    password: "<yourPasswordHere>",
-    host: "localhost",
-    port: 5432,
-    database: "jpms",
-}
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "jpms",
+};
